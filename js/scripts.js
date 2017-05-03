267e7a90a61a30ff6c1dac975c62c1bad4838263
jordan mysliwiec and kimlan nguyen <jordan+kimlan@>
2017-05-03 15:08:34 -0700

    separate consonants from words using splice method

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,9 @@ function translate(sentence){
   var modified_sentence = [];
   split_word.forEach(function(word){
     var letters = word.split("");
-    ;
-
+    var letter = letters[0];
+    var ascii_index = letter.charCodeAt(0);
+    // debugger;
     if(letters[0] === "A" || letters[0] === "E" || letters[0] === "I" || letters[0] === "O" || letters[0] === "U" || letters[0] === "a" || letters[0] === "e" || letters[0] === "i" || letters[0] === "o" || letters[0] === "u"){
       console.log(letters[0])
       //by default, word is not an array. Use .split on word to make it an array then store that value into word_array. Add "way" to word_array using push then join the array and remove ","
@@ -16,36 +17,48 @@ function translate(sentence){
       word_array.push("way");
       changed_word = word_array.join("");
       modified_sentence.push(changed_word);
-      // console.log(modified_sentence);
+      console.log(modified_sentence);
       // console.log(changed_word);
     }
-    else{
-      var letter = letters[0];
-      var ascii_index = letter.charCodeAt(0);
+    //None alphabetical characters
 
-      if(31<ascii_index<65 || 90<ascii_index<97 || 122<ascii_index<128){
-        modified_sentence.push(word);
-        console.log(modified_sentence);
-      }
-
-
-      // for(i=32; i<65; i++){
-      //   if (letters[0] === String.fromCharCode(i)){
-      //     modified_sentence.push(word);
-      //     console.log(modified_sentence);
-      //   }
-      // }
-
+    else if (32<=ascii_index && ascii_index <=64 || 90<ascii_index && ascii_index<97 || 122<ascii_index && ascii_index<128){
+      modified_sentence.push(word);
+      console.log(ascii_index);
+      console.log(modified_sentence);
     }
-    // else if(letters[0] === String.fromCharCode(35)) {
-    //   modified_sentence.push(word);
-    //   console.log(modified_sentence);
-    // }
+
+    //consonants
+    else if (letters[0] !== "q" && letters[0] !== "Q") {
+      var consonants= [];
+      // debugger;
+      for(i=0; i<letters.length; i++){
+        console.log(letters[i]);
+        debugger;
+
+        if(letters[i] !== "A" && letters[i] !== "E" && letters[i] !== "I" && letters[i] !== "O" && letters[i] !== "U" && letters[i] !== "a" && letters[i] !== "e" && letters[i] !== "i" && letters[i] !== "o" && letters[i] !== "u" ) {
+          //create an array to store consonants after each iteration
+
+          console.log(i);
+          consonants.push(letters[i]);
+
+
+          console.log(consonants);
+          // console.log(splice_letters);
+        }
+        if(letters[i] === "A" || letters[i] === "E" || letters[i] === "I" || letters[i] === "O" || letters[i] === "U" || letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u"){
+          var splice_letters = letters.splice(-(letters.length-i),letters.length-i)
+          console.log(splice_letters);
+          break
+        }
+
+
+      }
+    }
+
 
 
   });
-
-
 }
 
 
