03e2967cac686eb016dddd1073c434f608ba8ff2
jordan mysliwiec and kimlan nguyen <jordan+kimlan@>
2017-05-03 16:34:48 -0700

    add js code passing all specs.

--- js/scripts.js
+++ js/scripts.js
@@ -29,41 +29,56 @@ function translate(sentence){
     }
 
     //consonants
-    else if (letters[0] !== "q" && letters[0] !== "Q") {
+    else {
       var consonants= [];
       // debugger;
       for(i=0; i<letters.length; i++){
         console.log(letters[i]);
-        debugger;
 
         if(letters[i] !== "A" && letters[i] !== "E" && letters[i] !== "I" && letters[i] !== "O" && letters[i] !== "U" && letters[i] !== "a" && letters[i] !== "e" && letters[i] !== "i" && letters[i] !== "o" && letters[i] !== "u" ) {
-          //create an array to store consonants after each iteration
 
+          if (letters[0] ==="q" || letters[0] === "Q" && letters[1] === "u" || letters[1] === "U"){
+            var splice_letters = letters.splice(2);
+            var splice_letters_word = splice_letters.join("");
+            var concat_word = splice_letters_word.concat("qu") + "ay";
+            modified_sentence.push(concat_word)
+            console.log(concat_word);
+            break;
+          }
+
+          //if statement when first letter start with s or S
+          if (letters[0] === "s" || letters[0] === "S" && letters[1] ==="q" || letters[1] === "Q" && letters[2] === "u" || letters[2] === "U"){
+            var splice_letters = letters.splice(3);
+            var splice_letters_word = splice_letters.join("");
+            var concat_word = splice_letters_word.concat("squ") + "ay";
+            modified_sentence.push(concat_word);
+            console.log(modified_sentence);
+            break;
+          }
+          // create an array to store consonants after each iteration
+          else{
           console.log(i);
           consonants.push(letters[i]);
+          var consonants_word = consonants.join("");
 
-
-          console.log(consonants);
+          console.log(modified_sentence);
           // console.log(splice_letters);
+          }
         }
-        if(letters[i] === "A" || letters[i] === "E" || letters[i] === "I" || letters[i] === "O" || letters[i] === "U" || letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u"){
+        else if(letters[i] === "A" || letters[i] === "E" || letters[i] === "I" || letters[i] === "O" || letters[i] === "U" || letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u"){
           var splice_letters = letters.splice(-(letters.length-i),letters.length-i)
-          console.log(splice_letters);
+          var splice_letters_word = splice_letters.join("");
+          //concat two strings consonants_word & splice_letters_word
+          var concat_word = splice_letters_word.concat(consonants_word) + "ay";
+          modified_sentence.push(concat_word);
+          console.log(modified_sentence);
           break
         }
-
-
       }
     }
-
-
-
   });
+  return modified_sentence;
 }
-
-
-
-
 
 
 $(function(){
@@ -71,15 +86,9 @@ $(function(){
     event.preventDefault();
 
     array_input = $("input#user_input").val();
-    translate(array_input);
-    console.log(array_input)
-
-
-
-
-
-    // var modified_sentence
-    // $("#output").text(modified_sentence)
+    var pigLatin = translate(array_input);
+    var pigLatin_join = pigLatin.join(" ");
+    $("p#output").text(pigLatin_join);
 
   });
 });
